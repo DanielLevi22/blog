f14f626eb4d2cb051293d8408b123053b3b16067
Daniel Levi <daniellevi000@gmail.com>
2025-07-03 20:41:13 -0300

    feat: implementar funcionalidade de compartilhamento de posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,7 +7,13 @@ type BlogPostPage = {
     slug: string;
   }>;
 };
+export const revalidate = 60;
 
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 export default async function BlogPostPage({ params }: BlogPostPage) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
